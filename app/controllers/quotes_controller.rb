1df14077d849de8e6c4fa3d48452efcb74356326
Hannah Lee and Maggie Summers <maggie@>
2018-05-07 09:44:19 -0700

    add two more passing test specs for quote API integration

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -11,18 +11,26 @@ class QuotesController < ApplicationController
   end
 
   def create
-    @quote = Quote.create(quote_params)
-    json_response(@quote)
+    @quote = Quote.create!(quote_params)
+    json_response(@quote, :created)
   end
 
   def update
     @quote = Quote.find(params[:id])
-    @quote.update(quote_params)
+    if @quote.update!(quote_params)
+      render status: 200, json: {
+       message: "Your quote has been updated successfully."
+       }
+    end
   end
 
   def destroy
     @quote = Quote.find(params[:id])
-    @quote.destroy
+    if @quote.destroy!(quote_params)
+      render status: 200, json: {
+       message: "Your quote has been deleted successfully."
+       }
+    end
   end
 
   private
---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory(:quote) do
+    content('Hello World')
+    author('Kanye')
+  end
+end
